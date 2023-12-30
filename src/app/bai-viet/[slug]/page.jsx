8ebbf6d4833c7823320855b50bdb8cea48b8ba0a
Kinji <linhqwertyuiop2gmail.com>
2023-12-30 12:35:46 +0700

    update post page

--- src/app/bai-viet/[slug]/page.jsx
+++ src/app/bai-viet/[slug]/page.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import LeftPost from "../../../components/posts/leftPost/LeftPost";
+import PostContent from "../../../components/posts/postContent/PostContent";
+import CenterPost from "../../../components/posts/centerPost/CenterPost";
+import PostsPagination from "../../../components/pagination/postsPagination/PostsPagination";
 
+import "../page.css";
 const PostPage = () => {
-  return <div>Post</div>;
+  return (
+    <div className="main__container post">
+      <div className="row">
+        <div className="col c-8 row">
+          <div className="col c-2"></div>
+
+          <div className="col c-10 post__page">
+            <h1>
+              Ronaldo tỏ thái độ lạ sau khi trượt top 10 cầu thủ hay nhất thế
+              giới
+            </h1>
+            <div className="post__page--info">
+              <p className="posy__page--info-date">
+                Thứ bảy, 30/12/2023 - 09:30
+              </p>
+            </div>
+
+            <div className="post__page--desc">
+              <i>
+                (Dân trí) - Cristiano Ronaldo đã bày tỏ thái độ sau khi không có
+                tên trong danh sách 10 cầu thủ hay nhất thế giới năm 2023 do
+                Liên đoàn Thống kê và Lịch sử Bóng đá Thế giới (IFFHS) bình
+                chọn.
+              </i>
+            </div>
+
+            <div className="post__page--content">
+              <PostContent />
+            </div>
+          </div>
+
+          <div className="post__page--relate col c-12">
+            <p className="post__page--relate-title">Bài viết liên quan</p>
+            <CenterPost />
+            <PostsPagination />
+          </div>
+        </div>
+        <div className="col c-4">
+          <LeftPost />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
---
+++ src/components/posts/postContent/PostContent.jsx
@@ -0,0 +1,33 @@
+import "./postContent.css";
+
+const PostContent = () => {
+  return (
+    <div className="post__content">
+      <p>
+        "Tiền đạo Erling Haaland của Man City giành vị trí số một, trong khi
+        tiền đạo Kylian Mbappe của Paris Saint Germain và kình địch lâu năm của
+        Ronaldo, Lionel Messi, lần lượt xếp thứ hai và thứ ba trong top 10 cầu
+        thủ hay nhất thế giới theo công bố của IFFHS", trang A Bola (Bồ Đào Nha)
+        đăng trên Instagram hôm 29/12. Các ngôi sao của Man City là Rodri, Kevin
+        De Bruyne và Bernardo Silva cũng lọt vào top 10, cùng với Jude
+        Bellingham và Vinicius Junior của Real Madrid, Harry Kane của Bayern
+        Munich .
+      </p>
+      <br />
+      <p>
+        Các ngôi sao của Man City là Rodri, Kevin De Bruyne và Bernardo Silva
+        cũng lọt vào top 10, cùng với Jude Bellingham và Vinicius Junior của
+        Real Madrid, Harry Kane của Bayern Munich .
+      </p>
+      <br />
+      <p>
+        Đáng chú ý, Ronaldo đã bày tỏ cảm xúc của mình trong bài viết của A Bola
+        với 3 biểu tượng cười ra nước mắt và một con khỉ đang dùng hai bàn tay
+        che mắt nhưng không nói lời nào. Bình luận của CR7 nhận được hơn 220.000
+        lượt thích của người hâm mộ chỉ sau 16 tiếng, gấp ba bài gốc của báo.
+      </p>
+    </div>
+  );
+};
+
+export default PostContent;
